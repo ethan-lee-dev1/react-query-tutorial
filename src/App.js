f92cb6b4a29f026b575d7ee9a67d2d11f480e456
Ethan Lee <elee@xpanxion.com>
2023-05-30 20:39:09 -0400

    Comment ReactQueryDevtool because it is outdated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query-devtools";
+// import { ReactQueryDevtools } from "react-query-devtools";
 import Repositories from "./components/Repositories";
 
 const queryClient = new QueryClient({});
@@ -9,7 +9,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       {/* The rest of your application */}
       <Repositories />
-      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
     </QueryClientProvider>
   );
 };
